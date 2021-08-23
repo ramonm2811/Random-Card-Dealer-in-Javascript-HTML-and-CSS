04229fbad8e0f34959c84912da83a59007005ebb
ramonm2811 <ramonm2811@gmail.com>
2021-08-23 16:33:50 +0000

    card-generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,12 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let icons = ["heart text-danger", "gem", "spa", "crown"];
+  let randomIcon = Math.floor(Math.random() * icons.length);
+  let randomNumber = Math.floor(Math.random() * 10);
+  let iconParent = document.querySelectorAll(".icon");
+  document.querySelector(".number").innerHTML = `<span>${randomNumber}</span>`;
+
+  iconParent[0].innerHTML = `<i class="fas fa-${icons[randomIcon]}"></i>`;
+  iconParent[1].innerHTML = `<i class="fas fa-${icons[randomIcon]}"></i>`;
 };
